253c4ef1b9c793594f5627d51469cfe8f7a7be07
Mark Du <dulongjiebackup@gmail.com>
2023-07-21 09:34:15 -0400

    init commit

---
+++ src/main/java/com/spring/hplus/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.spring.hplus.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String goHome(){
+        System.out.println("home controller...");
+        return "index";
+    }
+}
---
+++ src/test/java/com/spring/hplus/HplusappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.hplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HplusappApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
